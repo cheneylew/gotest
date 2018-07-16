e2b5d2e013931350fce68addafd018b08eb9aa34
CheneyLew <cheneylew@163.com>
2018-07-16 21:10:42 +0800

    no message

--- test_gorm/main.go
+++ test_gorm/main.go
@@ -5,10 +5,20 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+type User struct {
+	Name string
+	Age float64
+	Height float64
+	Sex int8
+}
+
 func main() {
 	db, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		panic("failed to connect database")
 	}
 	defer db.Close()
+
+	db.AutoMigrate(&User{})
+	db.Create(&User{"Cheneylew", 18,176, 1})
 }
