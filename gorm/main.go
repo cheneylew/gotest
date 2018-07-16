e32f0920fb54da1348c3e600fe870c1197ecca79
Deju Liu <cheneylew@163.com>
2018-07-16 11:42:04 +0800

    no message

---
+++ gorm/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+}
